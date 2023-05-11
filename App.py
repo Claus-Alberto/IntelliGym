f333d857909a16cca18fe870148b97178268947c
Claus Alberto Bienemann <91323034+Claus-Alberto@users.noreply.github.com>
2023-05-11 11:55:36 -0300

    Add files via upload

---
+++ App.py
@@ -0,0 +1,47 @@
+from flask import Flask, render_template
+import json
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route("/register/")
+def register_screen():
+    return render_template('register.html')
+
+@app.route("/main/")
+def main():
+    return render_template('main.html', values = {
+        'week': {
+            'SEG':'check',
+            'TER':'check',
+            'QUA':'undone',
+            'QUI':'check',
+            'SEX':'check',
+            'SAB':'uncheck',
+            'DOM':'uncheck'
+        },
+        'training_name':'Pernas',
+        'training': {
+            'Leg Press' : {
+                'Rep' : '15',
+                'Series' : '4',
+                'Peso' : '250'
+            },
+            'Agachamento Livre' : {
+                'Rep' : '12',
+                'Series' : '4',
+                'Peso' : '80'
+            },
+            'Cadeira Extensora Unilateral' : {
+                'Rep' : '12',
+                'Series' : '4',
+                'Peso' : '40'
+            }
+        }
+    })
+
+if __name__ == '__main__':
+    app.run(debug=True)
