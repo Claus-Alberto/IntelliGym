6f758d87df1d41b6dffaa550a0e410c9982c5d02
Claus Alberto Bienemann <91323034+Claus-Alberto@users.noreply.github.com>
2023-05-11 03:33:19 -0300

    Add files via upload

--- Routes.py
+++ Routes.py
@@ -11,5 +11,37 @@ def index():
 def register_screen():
     return render_template('register.html')
 
+@app.route("/main/")
+def main():
+    return render_template('main.html', values = {
+        'week': {
+            'SEG':'check',
+            'TER':'check',
+            'QUA':'undone',
+            'QUI':'check',
+            'SEX':'check',
+            'SAB':'uncheck',
+            'DOM':'uncheck'
+        },
+        'training_name':'Pernas',
+        'training': {
+            'Leg Press' : {
+                'Rep' : '15',
+                'Series' : '4',
+                'Peso' : '250'
+            },
+            'Agachamento Livre' : {
+                'Rep' : '12',
+                'Series' : '4',
+                'Peso' : '80'
+            },
+            'Cadeira Extensora Unilateral' : {
+                'Rep' : '12',
+                'Series' : '4',
+                'Peso' : '40'
+            }
+        }
+    })
+
 if __name__ == '__main__':
     app.run(debug=True)
